74be7fbac6ef37f9adbecac8f66e517f79548007
Christian Landgren <cl@iteam.se>
2016-09-12 17:32:24 +0200

    updated examples and bugfix

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # timeparts
 
 Convert a date time to an object containing more searchable properties 
-such as day type (weekend, weekday) and time of day (afternoon, night, morning) 
-and also a special field called weekdayhour which is a compound number consisting 
+such as day type (weekend, weekday) and time of day (afternoon, night, morning)
+
+# weekdayhour
+Included in the datepart object there is also a special field called weekdayhour which is a compound number consisting 
 of the weekday number and hour, example: 111 is monday at 11am, 211 is tuesday at 11am, 
 623 is saturday at 11pm
 
+
+# how to install
+
+    $ npm install timeparts --save
+
+# how to use
+
+    var now = Date()
+    timeparts.
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 var moment = require('moment')
 
-module.exports = {
-  getTimeParts: function () {
-    var time = moment(new Date())
-    return {
-      time: time.format(),
-      hour: time.format('HH'),
-      weekday: time.format('dddd'),
-      timeofday: time.hour() < 4 && 'night' || time.hour() < 10 && 'morning' || time.hour() < 14 && 'day' || time.hour() < 18 && 'afternoon' || 'evening',
-      week: time.isoWeek(),
-      dayofmonth: time.format('DD'),
-      weekdayandhour: time.format('EHH'),
-      dayType: time.format('d') > 5 ? 'weekend' : 'weekday'
-    }
+module.exports = function (dateTime) {
+  var time = moment(dateTime || new Date())
+  return {
+    time: time.format(),
+    hour: time.format('HH'),
+    weekday: time.format('dddd'),
+    timeofday: time.hour() < 4 && 'night' || time.hour() < 10 && 'morning' || time.hour() < 14 && 'day' || time.hour() < 18 && 'afternoon' || 'evening',
+    week: time.isoWeek(),
+    dayofmonth: time.format('DD'),
+    weekdayandhour: time.format('EHH'),
+    dayType: time.format('d') > 5 ? 'weekend' : 'weekday'
   }
 }
+
